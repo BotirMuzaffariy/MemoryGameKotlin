0c30733a3bce28afab566530e2ecdbef7ca2d07d
Botir <lycr0343@gmail.com>
2022-10-22 15:56:39 +0500

    res folder fixed, navigation added

--- app/src/main/java/uz/lazydevv/memorygame/MainActivity.kt
+++ app/src/main/java/uz/lazydevv/memorygame/MainActivity.kt
@@ -1,11 +1,17 @@
 package uz.lazydevv.memorygame
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.Navigation.findNavController
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return findNavController(this, R.id.my_nav_host_container).navigateUp()
     }
 }
---
+++ app/src/main/java/uz/lazydevv/memorygame/models/CardM.kt
@@ -0,0 +1,6 @@
+package uz.lazydevv.memorygame.models
+
+data class CardM(
+    val imgId: Int,
+    val imgResId: Int
+)
